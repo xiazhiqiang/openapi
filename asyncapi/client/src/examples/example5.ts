4ce28c7d11cbd44d326d14ecbd8f34735ffca089
xiazhiqiang <xiazhiqiangcuc@126.com>
2023-06-29 23:44:01 +0800

    feat: 调试proxy代理逻辑,支持不同path的定制逻辑

--- asyncapi/client/src/examples/example5.ts
+++ asyncapi/client/src/examples/example5.ts
@@ -6,55 +6,51 @@ import { renderData } from "client/utils";
 export default () => {
   const url = "ws://127.0.0.1:3000/proxy/scene/getList?abc=111";
   const ws = new WebSocket(url);
+  let timer1: any = null;
+  ws.onopen = () => {
+    // 发送必要数据
+    ws.send(JSON.stringify({ msg: "w1连接成功！" }));
+
+    // 定时发送心跳请求
+    timer1 = setInterval(() => {
+      ws.send(JSON.stringify({ msg: "w1心跳数据" }));
+    }, 3000);
+  };
   ws.onmessage = (e) => {
     renderData(e.data);
   };
+  ws.onerror = (err) => {
+    console.log("err", err);
+  };
   ws.onclose = (e) => {
     console.log("close", e);
+    if (timer1) {
+      clearInterval(timer1);
+    }
     renderData("close connection");
   };
 
-  // // 不同路径的代理请求
-  // const url2 = "ws://127.0.0.1:3000/proxy/site/getCasesList?def=222";
-  // const ws2 = new WebSocket(url2);
-  // ws2.onmessage = (e) => {
-  //   renderData(e.data, "data2");
-  // };
-  // ws2.onclose = (e) => {
-  //   console.log("close", e);
-  //   renderData("close connection", "data2");
-  // };
+  // 不同路径的代理请求
+  const url2 = "ws://127.0.0.1:3000/proxy/site/getCasesList?def=222";
+  const ws2 = new WebSocket(url2);
+  let timer2: any = null;
+  ws2.onopen = () => {
+    // 发送必要数据
+    ws2.send(JSON.stringify({ msg: "ws2连接成功！" }));
 
-  // let heartbeatTimer: any = null;
-
-  // ws.onopen = () => {
-  //   // 发送必要数据
-  //   ws.send(
-  //     JSON.stringify({
-  //       abc: 111,
-  //     }),
-  //   );
-
-  //   // 定时发送心跳请求
-  //   heartbeatTimer = setInterval(() => {
-  //     ws.send(JSON.stringify({ msg: "心跳数据" }));
-  //   }, 3000);
-  // };
-
-  // ws.onerror = (error) => {
-  //   console.log("error", error);
-  // };
-
-  // ws.onmessage = (e) => {
-  //   // console.log("data", e.data);
-  //   renderData(e.data);
-  // };
-
-  // ws.onclose = (event) => {
-  //   console.log("close", event);
-  //   renderData("close connection");
-  //   if (heartbeatTimer) {
-  //     clearInterval(heartbeatTimer);
-  //   }
-  // };
+    // 定时发送心跳请求
+    timer2 = setInterval(() => {
+      ws2.send(JSON.stringify({ msg: "ws2心跳数据" }));
+    }, 3000);
+  };
+  ws2.onmessage = (e) => {
+    renderData(e.data, "data2");
+  };
+  ws2.onclose = (e) => {
+    console.log("close2", e);
+    if (timer2) {
+      clearInterval(timer2);
+    }
+    renderData("close connection", "data2");
+  };
 };
--- asyncapi/server/routes/proxy.js
+++ asyncapi/server/routes/proxy.js
@@ -16,43 +16,89 @@ router
     const interfaceQuery = req.query || {};
     const reqHeaders = req.headers || {};
 
-    // 根据路径及必要参数获取对应路径的请求定义、mock数据等
+    // console.log("jinlaile", interfacePath, interfaceQuery, reqHeaders);
 
-    let timer = null;
-    let interval = 1000; // 数据发送间隔时间，单位ms
-
-    // 定时推送数据给client
-    const sendData = ({ data = {}, interval }) => {
-      timer = setInterval(() => {
-        ws.send(
-          JSON.stringify({
-            code: 200,
-            data: {
-              ...data,
-              no: Math.round(Math.random() * 10e3),
-              timestamp: +new Date(),
+    const pathConfigMap = {
+      "/scene/getList": {
+        internal: 1000,
+        data: {
+          list: [
+            {
+              name: "轨迹",
             },
-          })
-        );
-      }, interval);
+            {
+              name: "灯态",
+            },
+          ],
+        },
+      },
+      "/site/getCasesList": {
+        internal: 2000,
+        data: {
+          list: [
+            {
+              caseName: "1111",
+            },
+            {
+              caseName: "2222",
+            },
+          ],
+        },
+      },
     };
 
-    sendData({ data: interfaceQuery, interval });
+    // 根据路径及必要参数获取对应路径的请求定义、mock数据等
+    let timer = null;
+
+    // 发送数据给client
+    const responseHandler = ({ query = {}, path = "" }) => {
+      const config = pathConfigMap[path];
+      if (!config) {
+        return;
+      }
+
+      const response = {
+        code: 200,
+        data: {
+          ...query,
+          ...(config.data || {}),
+          timestamp: +new Date(),
+        },
+      };
+
+      // 优先根据query中的interval设置向client发送数据频率
+      const internal = query.internal || config.internal;
+      if (internal) {
+        timer = setInterval(() => {
+          response.data.timestamp = +new Date();
+          ws.send(JSON.stringify(response));
+        }, internal);
+      } else {
+        ws.send(JSON.stringify(response));
+      }
+    };
+
+    responseHandler({ query: interfaceQuery, path: interfacePath });
 
     ws.on("message", function (msg) {
-      // let params = {};
-      // try {
-      //   // 解析message参数
-      //   params = JSON.parse(msg);
-      // } catch (e) {}
-      // // 根据message中的参数更新
-      // const { frameInterval } = params || {};
-      // if (frameInterval && frameInterval !== interval) {
-      //   interval = frameInterval;
-      //   clearInterval(timer);
-      //   timer = null;
-      //   sendData({ interval, data: req.query });
-      // }
+      const config = pathConfigMap[interfacePath];
+      let params = {};
+      try {
+        // 解析message参数
+        params = JSON.parse(msg);
+      } catch (e) {}
+
+      // 根据message中的参数更新
+      const { frameInterval } = params || {};
+      if (frameInterval && config && frameInterval !== config.internal) {
+        interval = frameInterval;
+        clearInterval(timer);
+        timer = null;
+        responseHandler({
+          path: interfacePath,
+          query: { ...interfaceQuery, ...params },
+        });
+      }
     });
 
     ws.on("close", function (id) {
